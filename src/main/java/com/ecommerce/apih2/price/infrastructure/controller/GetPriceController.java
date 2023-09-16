702d5f8710ab537198f0457f3d728dbc1a15ab73
nievefs <nievefernandezz2@gmail.com>
2023-09-16 20:47:36 +0200

    add swager

--- src/main/java/com/ecommerce/apih2/price/infrastructure/controller/GetPriceController.java
+++ src/main/java/com/ecommerce/apih2/price/infrastructure/controller/GetPriceController.java
@@ -4,16 +4,15 @@ import com.ecommerce.apih2.price.application.param.GetPriceParam;
 import com.ecommerce.apih2.price.application.usecase.GetPriceUseCase;
 import com.ecommerce.apih2.price.domain.error.ApiError;
 import com.ecommerce.apih2.price.domain.exception.PriceNotFoundException;
-import org.springframework.beans.TypeMismatchException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.annotation.AbstractNamedValueMethodArgumentResolver;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
 import java.time.format.DateTimeParseException;
 
@@ -26,6 +25,11 @@ public class GetPriceController {
         this.useCase = useCase;
     }
 
+    @Operation(summary = "Get price by params")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Price not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid date")})
     @GetMapping(value = "/final-price", produces = "application/json;charset=UTF-8" )
     public @ResponseBody ResponseEntity<?> getPrice(
             @RequestParam("date") String date,
@@ -41,10 +45,6 @@ public class GetPriceController {
                     new ApiError(HttpStatus.NOT_FOUND.value(), "THERE_IS_NO_PRICE")
             );
         }catch (DateTimeParseException e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                    new ApiError(HttpStatus.BAD_REQUEST.value(), "INVALID_DATE"));
-        }
-        catch (MethodArgumentTypeMismatchException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                     new ApiError(HttpStatus.BAD_REQUEST.value(), "INVALID_DATE"));
         }
